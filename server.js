758bb2a2b7b3bd48e51610021f832b6876a6c695
Your Nckkfclsme <you@example.com>
2025-05-26 18:29:23 +0530

    changed to serverless

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import menueRoutes from './routes/menueroutes.js'
 import dotenv from 'dotenv'
  dotenv.config();
 const PORT = process.env.PORT || 3000;
+import serverless from 'serverless-http'
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.get("/", (req, res) => {
 app.use('/person',personeRouter);
 app.use('/' , menueRoutes);
 
-app.listen(PORT, () => {
-  console.log("Listening  on port 3000");
-});
+// app.listen(PORT, () => {
+//   console.log("Listening  on port 3000");
+// });
+
+export const handler = serverless(app);
